341973950ad9c9c5246ccf0c59a25fa17f0069aa
Michael Stenta <mike@mstenta.net>
2024-04-12 13:04:06 -0400

    Depend on farm_convention.

--- modules/farm_surveystack_convention/farm_surveystack_convention.post_update.php
+++ modules/farm_surveystack_convention/farm_surveystack_convention.post_update.php
@@ -9,8 +9,16 @@
  * Install Asset Termination module.
  */
 function farm_surveystack_convention_post_update_enable_termination(&$sandbox = NULL) {
-  $module = 'farm_asset_termination';
   if (!\Drupal::service('module_handler')->moduleExists('farm_asset_termination')) {
     \Drupal::service('module_installer')->install(['farm_asset_termination']);
+  }
+}
+
+/**
+ * Install Convention module.
+ */
+function farm_surveystack_convention_post_update_enable_convention(&$sandbox = NULL) {
+  if (!\Drupal::service('module_handler')->moduleExists('farm_convention')) {
+    \Drupal::service('module_installer')->install(['farm_convention']);
   }
 }
